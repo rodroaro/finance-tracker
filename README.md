16a03c2a90e4cd7aaf8b0b7f8112a00c60023f01
rodroaro <rod_roaro_13@hotmail.com>
2022-05-27 16:07:53 -0500

    Add bootstrap for styling

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Finance tracker app made in Rails 6
+
+Dependencies:
+
+- Bootstrap with yarn:
+  yarn add bootstrap@4.6 jquery popper.js
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+// For bootstrap
+const webpack = require("webpack")
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}))
+
 module.exports = environment
